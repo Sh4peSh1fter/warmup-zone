bfcd6cc656f6a6a34bce66ab1942594b740bf266
Sean Sosis <shonsosis123@gmail.com>
2024-06-24 17:12:41 +0300

    improved word_consists_of_words

--- word_consists_of_words/main.py
+++ word_consists_of_words/main.py
@@ -1,22 +1,12 @@
 def is_word(text: str) -> bool:
-    list_of_words = ["dog", "i", "network", "love", "do", "go"]
-    for word in list_of_words:
+    words_list = ["dog", "i", "network", "love", "do", "go"]
+    for word in words_list:
         if word in text:
             return True
     return False
 
-def my_func(text: str) -> bool:
-    # input validation - is str
-    if not isinstance(text, str):
-        raise ValueError("text must be string")
-    # input validation - not empty
-    if not text:
-        raise ValueError("text cant be empty")
 
-    found_words: set[str] = set([])
-    uncertain_words: set[str] = set([])
-
-
+def find_suspicious_words(found_words: set[str], text: str) -> None:
     for starting_index in range(len(text)):
         temp_text: str = ""
         for char in text[starting_index:]:
@@ -27,26 +17,63 @@ def my_func(text: str) -> bool:
             if is_word(temp_text):
                 found_words.add(temp_text)
 
+
+def get_uncertain_words(uncertain_words: set, found_words: set) -> None:
     for sus_word in found_words:
         for other_sus_word in found_words:
             if other_sus_word != sus_word and other_sus_word in sus_word:
                 uncertain_words.add(sus_word)
 
-    # DEBUG
-    # print(f"suspicious words: {found_words}")
-    print(f"certain words: {found_words - uncertain_words}")
-    print(f"uncertain words: {uncertain_words}")
 
+def is_text_consist_of(text: str, words: set[str]) -> bool:
+    n: int = len(text)
+    dp: list[bool] = [False] * (n + 1)
+    dp[0] = True
+
+    for i in range(1, n + 1):
+        for j in range(i):
+            # # DEBUG
+            # print(f"i: {i}, j: {j}, dp[j]: {dp[j]}, text[j:i]: {text[j:i]}, is in: {text[j:i] in (found_words - uncertain_words)}")
+            if dp[j] and text[j:i] in words:
+                dp[i] = True
+                break
+        # # DEBUG
+        # print(f"dp: {dp}")
+    return dp[n]
+
+def my_func(text: str) -> None:
+    # input validation - is str
+    if not isinstance(text, str):
+        raise ValueError("text must be string")
+    # input validation - not empty
+    if not text:
+        raise ValueError("text cant be empty")
+
+    found_words: set[str] = set()       # words that contain words from the words_list
+    uncertain_words: set[str] = set()   # words that contain words from the words_list but might not be minimal - can contain other words within them.
+
+    find_suspicious_words(found_words, text)
+    get_uncertain_words(uncertain_words, found_words)
+
+    print(f"does \"{text}\" certainly consist of the words in the list?\n"
+          f"> {is_text_consist_of(text, found_words - uncertain_words)}")
+
+    print(f"does \"{text}\" consist of words that uncertainly in the list?\n"
+          f"> {is_text_consist_of(text, found_words)}")
+
+    # # DEBUG
+    # print(f"suspicious words: {found_words}")
+    # print(f"certain words: {found_words - uncertain_words}")
+    # print(f"uncertain words: {uncertain_words}")
 
 
 def main() -> None:
-    print(my_func("d"))   # True
-    print(my_func("dog"))   # True
-    print(my_func("dogo"))  # True
-    print(my_func("idog"))  # True
-    print(my_func("doig"))  # False
-    # print(my_func("lovenetworkingi"))   # False
-
+    my_func("d")        # False
+    my_func("dog")      # True
+    my_func("dogo")     # True
+    my_func("idog")     # True
+    my_func("doig")     # False
+    my_func("lovenetworkingi")  # False
 
 
 if __name__ == '__main__':
